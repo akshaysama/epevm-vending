f990a54e675b4f43035d6c3bde5449f3aa01a95d
akshaysama <akshay.sivan@vonnue.com>
2022-12-31 23:53:31 +0530

    fix: website bug

--- success.js
+++ success.js
@@ -1,15 +1,24 @@
+const processing = document.getElementsByClassName('processing')
+const Processing = document.querySelector('.processing')
 const loading = document.querySelector('.spinner')
 const SuccessText = document.getElementById('text')
 const button = document.getElementById('button')
+const payment = document.getElementById('payment')
+
 const removeLoading = ()=>{
     loading.remove()
+    Processing.remove()
+    payment.style.display = 'block'
     SuccessText.innerHTML='Your transaction is successful please collect the item'
     button.innerHTML = `<button  class="btn btn-success pull-right continue" style="margin-right: 5px; margin-top: 8px;"><a
     class=" text-white"><a href = 'index.html'>Continue Shopping</a></button>`
 
 }
 
-const myTimeout =  setTimeout( removeLoading, 5000);
-
+const removeSuccess = ()=>{
+    payment.remove()
+}
+const myTimeout =  setTimeout( removeLoading, 3500);
+const removeAnim = setTimeout(removeSuccess,4800)
 
 
